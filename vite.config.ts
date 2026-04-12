84f4c14fcb0186f53ee34194f0232b2ce1c3fdfb
Anees Manzoor <aneesmanzoor06@gmail.com>
2026-04-12 10:22:08 +0100

    chore: rename application title to Y&H Regional Criminal Seizure Analytics and created final file ready for client

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -14,6 +15,7 @@ export default defineConfig(({ mode }) => {
       plugins: [
         react(),
         tailwindcss(),
+        viteSingleFile(),
       ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
